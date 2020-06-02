0da8c32d6a48f44d7bce05469343bf610a8bdf49
root <root@4dd0be49fd35>
2020-06-02 21:57:38 +0000

    Day 2

--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -4,23 +4,27 @@ import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 public class FourSquare {
-	// 1. Call the drawSquare() method
 	
 	// 2. Create a new Robot
-
+	Robot bob = new Robot("batman");
+	
 	void go() {
 		// 4. Make the robot move as fast as possible
-
+		bob.setSpeed(20);
 		// 5. Set the pen width to 5
-
+		bob.penDown();
+		bob.setPenWidth(5);
 		// 6. Use a for loop to repeat steps #7 to #8, four times...
-
+		for(int i=0; i<4; i++) {
 			// 7. Set the pen color to random
-	
-			
-	
+		bob.setRandomPenColor();
+		
+		// 1. Call the drawSquare() method
+			drawSquare();
+		
 			// 8. Turn the robot 90 degrees to the right
-
+			bob.turn(90);
+		}
 	}
 
 	
@@ -29,34 +33,19 @@ public class FourSquare {
 		/* 3. Fill in the code to draw a square inside the method below. */
 		
 		
+		for (int i = 0; i<4; i++) {
+			bob.move(100);
+			bob.turn(90);
+		}
+		
 	}
 
 	public static void main(String[] args) {
 		new FourSquare().go();
-	Robot jarvis= new Robot();
-	jarvis.penDown();
-	jarvis.sparkle();
-	jarvis.setPenWidth(5);
-	jarvis.setSpeed(100);
-	jarvis.moveTo(300, 300);
-	for (int i = 0; i < 4; i++) {
-	jarvis.setRandomPenColor();
-	jarvis.move(200);
-	jarvis.turn(90);
-	jarvis.setRandomPenColor();
-	jarvis.move(200);
-	jarvis.turn(90);
-	jarvis.setRandomPenColor();
-	jarvis.move(200);
-	jarvis.turn(90);
-	jarvis.setRandomPenColor();
-	jarvis.move(200);
 	}
 	
-	}
-	
-	
-	}
+}
+
 
 
 
--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -6,10 +6,33 @@ import org.jointheleague.graphical.robot.Robot;
 public class MyFirstJavaProgram {
 	
 	public static void main(String[] args) {
-		
 		// START HERE
-	
+		Robot bob = new Robot("batman");
+		bob.setPenColor(Color.black);
+		bob.penDown();
+		bob.setSpeed(15);
+		bob.move(100);
+		bob.turn(90);
+		bob.setPenColor(Color.CYAN);
+		bob.move(100);
+		bob.hide();
+		bob.turn(90);
+		bob.penUp();
+		bob.move(200);
+		bob.turn(180);
+		bob.show();
+		bob.sparkle();
+		bob.hide();
+		bob.turn(270);
 		
+		Robot rob = new Robot();
+		rob.setSpeed(100);
+		rob.penDown();
 		
+		for(int i = 0; i < 4; i++ ) {
+			rob.move(100);
+			rob.turn(90);
+		}
 	}
+	
 }
